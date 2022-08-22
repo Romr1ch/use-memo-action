59a91db6965931db049077fdb095fe239f50a96d
Klyshnikov Roman <klyshnikov.roman@gmail.com>
2022-08-22 16:58:02 +0300

    Добавил тесты для reducer

---
+++ __tests__/reducer.test.ts
@@ -0,0 +1,47 @@
+import { memoReducer, MemoOption } from '../src'
+import { MEMO } from '../src/utils'
+
+describe('reducer', () => {
+  test('successful adding data only from hook', () => {
+    const action = {
+      type: '@@memo/USER/user',
+      payload: {
+        name: 'Username',
+        age: 42,
+      },
+      meta: {
+        memoOptions: {
+          key: 'user',
+          [MEMO]: true,
+        },
+      },
+    }
+
+    expect(memoReducer({}, action)).toEqual({
+      [action.meta.memoOptions.key]: action.payload,
+    })
+  })
+
+  test('other action without adding', () => {
+    const action = {
+      type: 'OTHER_ACTION',
+      payload: 42,
+    }
+
+    expect(memoReducer({}, action)).toEqual({})
+  })
+
+  test('failed attempt to replace hook action', () => {
+    const action = {
+      type: '@@memo/USER/42',
+      payload: 42,
+      meta: {
+        memoOptions: {
+          key: 'user',
+        } as MemoOption,
+      },
+    }
+
+    expect(memoReducer({}, action)).toEqual({})
+  })
+})
